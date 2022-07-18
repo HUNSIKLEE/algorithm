6f022a079e7d8f9a3650783d71b0247c46adfdb9
HUNSIKLEE <hunsik0206@gmail.com>
2022-07-18 23:07:21 +0900

    .

--- day/day0717.java
+++ day/day0717.java
@@ -1,10 +1,14 @@
-public class day0716 {
+public class day0717 {
+
 	public static void main(String[] args) {
 		java.util.Scanner keyIn = new java.util.Scanner(System.in);
 		int a,b;
 		a = keyIn.nextInt();
 		b = keyIn.nextInt();
 		System.out.println( a * b );
+	
+	
+	
 	}
 
 }
---
+++ day/day0718.java
@@ -0,0 +1,14 @@
+public class day0718 {
+
+	public static void main(String[] args) {
+		int a = 7, b = 3;
+		
+		System.out.println( a + b );
+		System.out.println( a - b );
+		System.out.println( a * b );
+		System.out.println( a / b );
+		System.out.println( a % b );
+	
+	}
+
+}
