b8f181bcdb1ee0a6abc106450f38e9100c5c6e3d
HUNSIKLEE <hunsik0206@gmail.com>
2022-07-19 00:04:45 +0900

    .

---
+++ day/day0718-1.java
@@ -0,0 +1,11 @@
+public class day0719 {
+	
+	  public static void main(String[] args) {
+	
+		java.util.Scanner keyIn = new java.util.Scanner(System.in);
+
+		String j = keyIn.nextLine();
+		System.out.printf(j+"??!");
+	
+	  }
+	}
